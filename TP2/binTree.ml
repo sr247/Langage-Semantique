19e580e30a6223ddbb06c4f40475e3e5a3e2d542
sr247 <stephane@Optimus-Prime.com>
2017-09-28 10:00:47 +0200

    Avancé sur le TP2 : Continuations sur la fonction Find.

--- TP2/binTree.ml
+++ TP2/binTree.ml
@@ -1,3 +1,4 @@
+exception Element_Found
 type 'a tree =
   | Leaf
   | Node of 'a * 'a tree * 'a tree
@@ -20,10 +21,19 @@ Utiliser deux continuations pour distinguer le cas ou l'élément a été trouv�
 Question3 :  écrire en CPS une fonction [find : a -> a tree -> a list options] telle que, si [a] existe dans [t] alors [find a t] renvoie la liste des noeuds sur le chemin de la racine vers [a]
 *)
 
-     
-(* Question 1 : *)
+let atree = Node (2, Node (5, Leaf, Leaf), Node (3, Leaf, Leaf))
+(* Question 1 : renvoyer la liste des noeuds en parcours dfs *)
+let cps_dfs_test (t: int tree) =
+  let rec dfs_aux (t: 'a tree) (k: 'a list -> 'a list) : 'a list =
+    match t with
+    | Leaf -> k []
+    | Node(a, t, u) -> dfs_aux t (fun dfst -> dfs_aux u (fun dfsu -> k (a :: dfst @ dfsu)) )
+  in
+  dfs_aux t (fun x -> x)
+
+(* Question 1 : afficher la liste des noeuds en dfs *)
 let cps_dfs (t: int tree) : unit =
-  let rec dfs_aux (t: 'a tree) (k: 'a list -> 'b) =
+  let rec dfs_aux (t: 'a tree) (k: 'a list -> 'b)  : 'b=
     match t with
     | Leaf -> k []
     | Node(a, t, u) -> dfs_aux t (fun dfst -> dfs_aux u (fun dfsu -> k (a :: dfst @ dfsu)) )
@@ -34,20 +44,65 @@ let cps_dfs (t: int tree) : unit =
 (* Question 2 : *)
 (* Ici jouer avec des listes n'est peut etre pas la meilleure solution *)
 (* Envisager de jouer peut-etre directement avec les trees *)
-let cps_print_before (e: int) (t: int tree) : unit =
-  let rec cps_print_aux (e': int) (t: int tree) (k1: 'a list -> unit) (k2: 'a list -> unit) =
-  match t with
-  | Leaf -> ()
-  | Node(a, s, u) -> cps_print_aux e' (fun prntL -> k1  
-  (* | Node(a, t, u) -> if e = a then print_string a; cps_print_aux e (fun  *)
+(* let cps_print_before (e: int) (t: int tree) = *)
+(*   let rec cps_print_aux (t: 'a tree) (k1: 'a -> unit) (k2: 'a -> unit) = *)
+(*     match t with *)
+(*     | Leaf -> k1 0 (\* Printf.printf "Missed " *\)  *)
+(*     | Node(a, t, u) -> (\* Printf.printf "A value %d, " a; *\) *)
+(*        cps_print_aux t (fun prntT -> cps_print_aux u (fun prntU -> if e <> a then k1 a else k2 a) (fun x -> ())) (fun noExectpion -> ()) *)
+(*  in *)
+(*  cps_print_aux t print_int (fun x -> raise Element_Found) *)
+
+let cps_print_before_One_k (e: string) (t: string tree) =
+  let rec cps_print_aux (t: 'a tree) (k1: 'a -> 'b) : 'b =
+    match t with
+    | Leaf -> ()
+    | Node(a, t, u) ->
+       if a = e
+       then raise Element_Found else
+         begin
+           cps_print_aux u (fun at -> cps_print_aux t (fun ut -> k1 ut); k1 at); k1 a
+         end
+  in
+  cps_print_aux t print_string
+
 (* Deux continuation ici : soit on est différent de a alors on print
 Des qu'uon est égal à a : on fait la continuation qui try with : StopIteration *)
-     
-(* Question 3 : indication    *)       
-(* let double_barrel_cps_find a t = *)
-(*   let rec find t kf knf = *)
-(*     match t with *)
-(*     | Leaf -> knf [] *)
-(*     | Node(e, t, u) -> *)
-(*        if e = a then find t (fun fnd -> *)
-(*          cps (evaluateur lambda) *)
+(* cps_print_before 5 Node(2, Node(5, Leaf, Leaf), Node(3, Leaf, Leaf)) *)
+(* Test *)
+
+(* let btree = Node("2", Node("6", Leaf, Leaf), Node("3", Node("8", Leaf, Node("4", Leaf, Leaf)), Leaf) ) *)
+(* let () = cps_print_before_One_k "5" btree *)
+
+
+                                
+(* Question 3 : indication *)
+exception Found of int list
+let find a t =
+  let rec find t = match t with
+    | Leaf -> ()
+    | Node(b, g, d) ->
+       if a = b
+       then raise (Found [])
+       else
+	 try find g; find d
+	 with Found(path) -> raise (Found(b::path))
+  in
+  try find t; None
+  with Found(path) -> Some path
+                           
+(* let double_barrel_cps_find (a:string) (t:string tree) = *)
+(*   let rec find (t:'a tree) (kf:'a -> unit) (knf:'a -> unit) = *)
+let double_barrel_cps_find a t =
+  let rec find t kf knf =
+    match t with
+    | Leaf -> knf () (*  Soit je renvoi unit, soit je renvoi continuation ( qui fera unit).. à voir *)
+    | Node(e, g, d) ->
+       find d (fun tlist -> if e = a then raise (Found glist) else try find g kf knf with Found(pathlist) -> raise (Found(e::pathlist)));kf []
+  in
+  try
+    find t (fun x -> raise (Found [x])) (fun y -> y)
+  with Found(path) -> Some path
+
+(* let ctree = Node (2, Node (5, Leaf, Leaf), Node (3, Leaf, Node(4,Leaf, Leaf))) *)
+(* let () = double_barrel_cps_find 4 ctree *)
