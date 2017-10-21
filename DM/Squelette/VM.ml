8d38a35eade70824a79ab2f4d62b227ea69c2025
Stéphane Romany <sr247@users.noreply.github.com>
2017-10-22 00:48:01 +0200

    Update VM.ml

--- DM/Squelette/VM.ml
+++ DM/Squelette/VM.ml
@@ -80,7 +80,6 @@ let step state =
   | IS.MkClos(id, c') ->
      let v = Closure(id, c') in
      push(v)
-<<<<<<< Updated upstream
   | IS.Apply ->
      let Closure(id, c') = pop() in
      (* Ici je devrait éventuellement matcher le pop...*)
@@ -93,12 +92,6 @@ let step state =
   | IS.Return ->
      let v = pop() in
      ()
-=======
-  (* | IS.Apply -> *)
-  (*    let Closure(id, c') = pop () in *)
-  (*    step c' *)
-       
->>>>>>> Stashed changes
   | _ -> failwith "Not implemented"
 
 (**
